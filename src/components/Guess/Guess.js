0a8cbb52fcc48c5c9287f4a3f83e884976a4c8bb
Felix Uhl <felix.uhl@netlight.com>
2023-03-24 16:49:55 +0100

    Exercise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ guess = "" }) {
+function Guess({ guess, answer }) {
+  const letters = guess
+    ? checkGuess(guess, answer)
+    : range(5).map(() => ({ letter: "", status: "" }));
+
   return (
     <p className="guess">
-      {range(0, 5).map((index) => (
-        <span className="cell" key={index}>
-          {guess.at(index)}
+      {letters.map(({ letter, status }, index) => (
+        <span className={`cell ${status}`} key={index}>
+          {letter}
         </span>
       ))}
     </p>
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,7 +3,7 @@ import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
 import Guess from "../Guess/Guess";
 
-function GuessResults({ guesses = ["DEFAU", "GUESS"] }) {
+function GuessResults({ guesses = ["DEFAU", "GUESS"], answer }) {
   const displayedGuesses = [
     ...guesses,
     ...range(guesses.length, NUM_OF_GUESSES_ALLOWED).map(() => ""),
@@ -12,7 +12,7 @@ function GuessResults({ guesses = ["DEFAU", "GUESS"] }) {
   return (
     <div className="guess-results">
       {displayedGuesses.map((guess, index) => (
-        <Guess guess={guess} key={index}></Guess>
+        <Guess guess={guess} answer={answer} key={index}></Guess>
       ))}
     </div>
   );
